19670438612e931ddeb0915cf3ab68524cb16667
Carlos Eduardo Tavares <carlos_ed.tavares@hotmail.com>
2022-04-29 12:33:09 -0300

    Subindo Versao base com Responsividade

---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto Site de Streaming
+
+### Adcionado Campo de Pequisa
+
+### Adicionado um Menu de Navegacao
+
+### Adicionado Botoes com Efeitos e Sombras
+
+### Adiconado no Rodape Icones que redirecionam em uma outra aba para meu Github e Linkedin
+
+### Adicionado Responsividade 
---
+++ js/wol/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:false,
+    dots: false,
+    responsive:{
+        600:{
+            items:3
+        },
+        800:{
+            items:3.5
+        },
+        1000:{
+            items:4
+        }
+    }
+    
+    
+})
+
